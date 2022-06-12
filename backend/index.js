bb181aa75f4c422cfe8136873f82343a11553b93
Bulat15 <mazikazik64@gmail.com>
2022-06-12 08:17:47 +0300

    полноценная основа бека и черновики проекта

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log('привет');
+const express = require("express");
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello world!');
+})
+
+
+app.listen(PORT, ()=>{
+    console.log('Server started...')
+})
